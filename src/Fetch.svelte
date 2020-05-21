6f185c27295a35dbd58b9e7bd00d8c9a6b738ef4
Peng <Jeff.Peng@acer.com>
2020-05-21 17:36:58 +0800

    first-commit

---
+++ src/Fetch.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { onMount } from "svelte";
+  let data = [];
+  onMount(async function() {
+    const response = await fetch("https://academy.valentinog.com/api/link/");
+    const json = await response.json();
+    data = json;
+  });
+</script>
