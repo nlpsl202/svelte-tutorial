443c5c5cd5b8416bebbbcef349cd469f39a8cf5b
Peng <Jeff.Peng@acer.com>
2020-06-11 16:48:46 +0800

    '球'

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,23 @@
 <script>
   export let name;
-  import ChatBot from "./ChatBot.svelte";
   import Table from "@nlpsl202/svelte-data-tabu";
   import { HeaderConfig } from "./config/HeaderConfig.js";
   import Fetch from "./Fetch.svelte";
   import Form from "./Form.svelte";
   import Video from "./Video.svelte";
-  import Logo from "./Logo.svelte";
+  import ChatBot from "./ChatBot.svelte";
+  import ToyBall from "./ToyBall.svelte";
   let searchTerm;
   let pinToTop = true;
   function handleSubmit() {
     const { value } = this.elements.search;
     searchTerm = value;
   }
+
+  import { spring } from 'svelte/motion';
+
+	let coords = spring({ x: 50, y: 50 });
+	let size = spring(10);
 </script>
 
 <style>
@@ -35,9 +40,32 @@
       max-width: none;
     }
   }
+
+  svg { width: 100%; height: 100%; margin: -8px; }
+	circle { fill: #ff3e00 }
 </style>
 
+<div style="position: absolute; right: 1em;">
+	<label>
+		<h3>stiffness ({coords.stiffness})</h3>
+		<input bind:value={coords.stiffness} type="range" min="0" max="1" step="0.01">
+	</label>
+
+	<label>
+		<h3>damping ({coords.damping})</h3>
+		<input bind:value={coords.damping} type="range" min="0" max="1" step="0.01">
+	</label>
+</div>
+<svg
+	on:mousemove="{e => coords.set({ x: e.clientX, y: e.clientY })}"
+	on:mousedown="{() => size.set(30)}"
+	on:mouseup="{() => size.set(10)}"
+>
+	<circle cx={$coords.x} cy={$coords.y} r={$size}/>
+
 <main>
+
+
   <h1>Hello {name}!</h1>
   <p>
     Visit the
@@ -49,7 +77,7 @@
   </div>
   <Form {handleSubmit} />
   <Fetch {searchTerm} let:data>
-    <h1>A list of links</h1>
+    <h1>LIST</h1>
     <!-- <ul>
       {#each data as link}
         <li>
@@ -60,3 +88,4 @@
     <Table title="Table Title" config={HeaderConfig} {data} {pinToTop} />
   </Fetch>
 </main>
+</svg>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: '測試修改git喔'
+		name: '看屁'
 	}
 });
 
